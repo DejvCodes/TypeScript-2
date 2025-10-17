1dcdeff07cbb10c9ea4ce1af081425680712f77c
David Kalmus <dejvios@seznam.cz>
2025-10-17 13:05:41 +0200

    advanced-ts

---
+++ 01_what_is_TS/script.ts
@@ -0,0 +1,17 @@
+/*
+Co je to TypeScript
+• TypeScript (zkráceně TS) je superset JavaScriptu – což znamená, že rozšiřuje JavaScript o 
+  nové vlastnosti, ale zároveň je plně kompatibilní s běžným JavaScriptem
+
+Jednoduše řečeno
+• každý platný JS kód je zároveň platný TypeScriptový kód, ale ne naopak – TypeScript přidává 
+  navíc hlavně statické typování a nástroje pro lepší kontrolu chyb už při psaní kódu
+
+
+Funkce	       ->   JavaScript	                             TypeScript
+Typování	     ->   • dynamické (typy se určují až za běhu)	 • statické (typy se kontrolují při kompilaci)
+Kontrola chyb  ->   • až při spuštění	                       • už při psaní/kompilaci
+Podpora OOP	   ->   • omezená	                               • silnější (třídy, rozhraní, generika, moduly)
+Nástroje	     ->   • základní	                             • výborná integrace s VS Code, IntelliSense, refaktoring
+Výstup	       ->   • spouští se přímo v prohlížeči	         • kompiluje se do čistého JavaScriptu
+*/
---
+++ 02_installation/script.ts
@@ -0,0 +1,23 @@
+/*
+1. Kontrola, že máme Node.js a npm
+• node -v
+• npm -v
+
+2. Instalace TypeScriptu
+• npm install -g typescript
+
+3. Ověření instalace
+• tsc -v
+
+4. Vytvoření nového projektu
+• mkdir ts-test
+• cd ts-test
+• npm init -y
+
+5. Nainstaluj TS lokálně (doporučeno pro projekty)
+• npm install --save-dev typescript
+
+6. Přeložení TypeScriptu do JavaScriptu
+• npx tsc
+• npx tsc -w (sledovat)
+*/
