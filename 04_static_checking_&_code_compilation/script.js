8e2cc19294a720af65d7d7d72a0340bf6ab642d3
David Kalmus <dejvios@seznam.cz>
2025-10-19 11:00:12 +0200

    ts

---
+++ 04_static_checking_&_code_compilation/script.js
@@ -0,0 +1,6 @@
+var student = {
+    firstName: 'David',
+    secondName: 'Kalmus'
+};
+// console.log(student.firstNamaaes) // static checking - upozorní na chybu
+console.log(student.firstName);
---
+++ 04_static_checking_&_code_compilation/script.ts
@@ -0,0 +1,7 @@
+const student = {
+    firstName: 'David',
+    secondName: 'Kalmus'
+}
+
+// console.log(student.firstNamaaes) // static checking - upozorní na chybu
+console.log(student.firstName)
