cfaf0bdf4e49b7112119a16dc95c1d8b418233f9
David Kalmus <dejvios@seznam.cz>
2025-10-20 15:25:22 +0200

    ts

---
+++ 07_any/script.ts
@@ -0,0 +1,19 @@
+/*
+Datový typ any
+• any znamená doslova -> libovolný typ
+• když proměnnou označíš jako any, TypeScript vypne kontrolu typů — můžeš 
+  do ní přiřadit cokoli a dělat s ní cokoli, bez chyb
+
+Nevýhoda any!
+• použitím any přicházíš o všechny výhody TS — ztrácíš typovou bezpečnost
+
+Kdy má any smysl?
+• když pracuješ s neznámým externím kódem nebo knihovnou, která nemá typy
+• když rychle prototypuješ a typy tě brzdí
+*/
+let something: any = 'David';
+something = 40;
+something = true;
+something = null;
+
+console.log(something);
