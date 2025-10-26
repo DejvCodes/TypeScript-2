9feb53cd91ba5952f33f5fc570d0379115af10ad
David Kalmus <dejvios@seznam.cz>
2025-10-26 16:55:52 +0100

    ts

---
+++ 16_object_as_return_value/script.js
@@ -0,0 +1,11 @@
+// Objekt jako návratová hodnota
+var randomCoordinate = function () {
+    return {
+        x: Math.random(),
+        y: Math.random()
+        // z: Math.random() // 'z' does not exist in type 
+    };
+};
+console.log(randomCoordinate()); // {x: 0.49694866575439745, y: 0.09686178038282423}
+console.log(randomCoordinate().x); // 0.9483650842646539
+console.log(randomCoordinate().y); // 0.034579584448909295
